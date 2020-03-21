c43551760532cfc675de547c7d55502f5e1239e9
John Polyzos <johnpolyzos@192.168.0.131>
2020-03-21 17:13:50 +0200

    Player, Stage custom hooks

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import { createStage } from '../gameHelpers';
+import React, { useState } from 'react';
+
+//styled components
 import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris';
+
+//custom hooks
+import { usePlayer } from '../hooks/usePlayer';
+import { useStage } from '../hooks/useStage';
+
+//components
 import Stage from './Stage';
 import Display from './Display';
 import StartButton from './StartButton';
 
 
 const Tetris = () => {
-    return (
-        <StyledTetrisWrapper>
-          <StyledTetris>
-            <Stage stage={createStage()}/>
-            <aside>
+  const [dropTime, setDropTime] = useState(null);
+  const [gameOver, setGameOver] = useState(false);
+
+  const [player] = usePlayer();
+  const [stage, setStage] = useStage(player);
+
+  console.log('re-render');
+
+  return (
+      <StyledTetrisWrapper>
+        <StyledTetris>
+          <Stage stage={stage}/>
+          <aside>
+              {gameOver ? (
+                <Display gameOver={gameOver} text="Game Over" />
+              ) : (
                 <div>
-                    <Display text="score"/>
-                    <Display text="Rows"/>
-                    <Display text="Level"/>
+                  <Display text="score"/>
+                  <Display text="Rows"/>
+                  <Display text="Level"/>
                 </div>
-                <StartButton />
-            </aside>
-          </StyledTetris>
-        </StyledTetrisWrapper>
+              )}              
+              <StartButton />
+          </aside>
+        </StyledTetris>
+      </StyledTetrisWrapper>
     )
 }
 
